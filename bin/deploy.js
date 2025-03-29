ea8adfb17376d562b3729425c789f0b3b49c9494
Seth Battis <>
2025-03-29 19:47:17 -0400

    build: add Firestore to deploy

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,6 +1,7 @@
 import gcloud from '@battis/partly-gcloudy';
 import { Core } from '@battis/qui-cli.core';
 import { Root } from '@battis/qui-cli.root';
+import { Shell } from '@battis/qui-cli.shell';
 import path from 'node:path';
 
 (async () => {
@@ -22,6 +23,15 @@ import path from 'node:path';
   });
 
   if (configure) {
+    await gcloud.services.enable(gcloud.services.API.CloudFirestoreAPI);
     await gcloud.services.enable(gcloud.services.API.CloudLoggingAPI);
+    const [{ name: database }] = JSON.parse(
+      Shell.exec(
+        `gcloud firestore databases list --project=${project.projectId} --format=json --quiet`
+      )
+    );
+    Shell.exec(
+      `gcloud firestore databases update --type=firestore-native --database="${database}" --project=${project.projectId} --format=json --quiet`
+    );
   }
 })();
