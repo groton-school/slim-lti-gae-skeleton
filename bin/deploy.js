a7bbe2b859b453fb860c71ceeb815d98b997a8aa
Seth Battis <sbattis@groton.org>
2025-04-17 15:45:56 -0400

    revert: reorder sequence of deploy
    This reverts commit e8d7360b6628517c7ea74ca84c05d0d61812522d.

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,24 +1,28 @@
-import gcloud from "@battis/partly-gcloudy";
-import { Core } from "@battis/qui-cli.core";
-import { Root } from "@battis/qui-cli.root";
-import { Shell } from "@battis/qui-cli.shell";
-import path from "node:path";
+import gcloud from '@battis/partly-gcloudy';
+import { Core } from '@battis/qui-cli.core';
+import { Root } from '@battis/qui-cli.root';
+import { Shell } from '@battis/qui-cli.shell';
+import path from 'node:path';
 
 (async () => {
   Root.configure({ root: path.dirname(import.meta.dirname) });
   const {
-    values: { force },
+    values: { force }
   } = await Core.init({
     flag: {
       force: {
-        short: "f",
-        default: false,
-      },
-    },
+        short: 'f',
+        default: false
+      }
+    }
+  });
+  const configure = force || !process.env.PROJECT;
+
+  const { project } = await gcloud.batch.appEngineDeployAndCleanup({
+    retainVersions: 2
   });
 
-  if (force || !process.env.PROJECT) {
-    await gcloud.services.enable(gcloud.services.API.CloudLoggingAPI);
+  if (configure) {
     await gcloud.services.enable(gcloud.services.API.CloudFirestoreAPI);
     await gcloud.services.enable(gcloud.services.API.CloudLoggingAPI);
     const [{ name: database }] = JSON.parse(
@@ -30,8 +34,4 @@ import path from "node:path";
       `gcloud firestore databases update --type=firestore-native --database="${database}" --project=${project.projectId} --format=json --quiet`
     );
   }
-
-  const { project } = await gcloud.batch.appEngineDeployAndCleanup({
-    retainVersions: 2,
-  });
 })();
