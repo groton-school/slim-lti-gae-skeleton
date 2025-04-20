d6659fc21c3dd9baa4e7ed753041f3b375f48219
Seth Battis <sbattis@groton.org>
2025-04-19 20:46:33 -0400

    feat: add LTI registration URL to initial deploy directions

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,25 +1,27 @@
-import gcloud from '@battis/partly-gcloudy';
-import { Core } from '@battis/qui-cli.core';
-import { Root } from '@battis/qui-cli.root';
-import { Shell } from '@battis/qui-cli.shell';
-import path from 'node:path';
+import gcloud from "@battis/partly-gcloudy";
+import { Colors } from "@battis/qui-cli.colors";
+import { Core } from "@battis/qui-cli.core";
+import { Log } from "@battis/qui-cli.log";
+import { Root } from "@battis/qui-cli.root";
+import { Shell } from "@battis/qui-cli.shell";
+import path from "node:path";
 
 (async () => {
   Root.configure({ root: path.dirname(import.meta.dirname) });
   const {
-    values: { force }
+    values: { force },
   } = await Core.init({
     flag: {
       force: {
-        short: 'f',
-        default: false
-      }
-    }
+        short: "f",
+        default: false,
+      },
+    },
   });
   const configure = force || !process.env.PROJECT;
 
-  const { project } = await gcloud.batch.appEngineDeployAndCleanup({
-    retainVersions: 2
+  const { project, appEngine } = await gcloud.batch.appEngineDeployAndCleanup({
+    retainVersions: 2,
   });
 
   if (configure) {
@@ -34,4 +36,14 @@ import path from 'node:path';
       `gcloud firestore databases update --type=firestore-native --database="${database}" --project=${project.projectId} --format=json --quiet`
     );
   }
+
+  Log.info(
+    `Install your LTI by going adding an LTI Registration in Developer Keys for ${Colors.url(
+      `https://${appEngine.defaultHost}/lti/register`
+    )}\n\nIf you haven't done that before, follow these directions: ${Colors.url(
+      "https://community.canvaslms.com/t5/Admin-Guide/How-do-I-add-a-developer-LTI-Registration-key-for-an-account/ta-p/601370"
+    )}\n\nYou will then need to enable the app following these directions: ${Colors.url(
+      "https://community.canvaslms.com/t5/Admin-Guide/How-do-I-configure-an-external-app-for-an-account-using-a-client/ta-p/202"
+    )}`
+  );
 })();
