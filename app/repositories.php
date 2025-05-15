eb3063eec0ddab5dbbfb1c7217406aa356830d0f
Seth Battis <sbattis@groton.org>
2025-05-15 15:04:27 -0400

    feat: store user data in Firestore

--- app/repositories.php
+++ app/repositories.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Domain\LTI\LaunchDataRepositoryInterface;
+use App\Domain\User\UserRepositoryInterface;
+use App\Infrastructure\Persistence\FirestoreUserRepository;
+use App\Infrastructure\Session\SessionLaunchDataRepository;
 use DI\ContainerBuilder;
 
-return function (ContainerBuilder $containerBuilder) {};
+return function (ContainerBuilder $containerBuilder) {
+    $containerBuilder->addDefinitions([
+        LaunchDataRepositoryInterface::class => DI\autowire(SessionLaunchDataRepository::class),
+        UserRepositoryInterface::class => DI\autowire(FirestoreUserRepository::class),
+    ]);
+};
